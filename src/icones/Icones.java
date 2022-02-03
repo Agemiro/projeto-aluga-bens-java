f710563edbd802d9da88e3354469118e6ad5a451
Agemiro <agemiroalves11@gmail.com>
2022-02-03 10:43:25 -0300

    Criada pasta exclusiva pra imagens

---
+++ src/icones/Icones.java
@@ -0,0 +1,22 @@
+package icones;
+
+import javax.swing.ImageIcon;
+
+public class Icones {
+	
+	public static final ImageIcon IMAGEM = new ImageIcon("images/imagem.png");
+	public static final ImageIcon SETA = new ImageIcon("images/seta.png");
+	public static final ImageIcon CADEADO_FECHADO = new ImageIcon("images/cadeado_fechado.png");
+	public static final ImageIcon REINICIAR = new ImageIcon("images/icon_reiniciar.png");
+	public static final ImageIcon OK = new ImageIcon("images/ok.png");
+	public static final ImageIcon CANCELAR = new ImageIcon("images/icon_cancel.png");
+	public static final ImageIcon LOGIN = new ImageIcon("images/login-icon.png");
+	public static final ImageIcon LOGIN2 = new ImageIcon("images/icone_login.png");
+	public static final ImageIcon CADASTRO = new ImageIcon("images/cadastro.png");
+	public static final ImageIcon EDITAR = new ImageIcon("images/icon_editar.png");
+	public static final ImageIcon VOLTAR = new ImageIcon("images/icon_voltar.png");
+	public static final ImageIcon ENVIAR_EMAIL = new ImageIcon("images/icon_email.png");
+	public static final ImageIcon IMAGEM_FUNDO2 = new ImageIcon("images/esquina.png");
+	public static final ImageIcon PDF_IMAGE = new ImageIcon("images/icon_pdf.png");
+	
+}
